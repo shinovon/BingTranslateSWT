af7a208495aa9a4e041ce07c0435b18f4d72a6d1
Shinovon <arman200711@gmail.com>
2021-08-24 02:46:21 +0500

    рабочий lcdui интерфейс

--- src/cc/nnproject/translate/bing/lcdui/TranslateUIBingLCD.java
+++ src/cc/nnproject/translate/bing/lcdui/TranslateUIBingLCD.java
@@ -6,37 +6,69 @@ import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.ItemCommandListener;
+import javax.microedition.lcdui.List;
+import javax.microedition.lcdui.StringItem;
 import javax.microedition.lcdui.TextField;
 
 import cc.nnproject.translate.ITranslateUI;
 import cc.nnproject.translate.bing.TranslateBingMIDlet;
 import cc.nnproject.translate.bing.TranslateBingThread;
 
-public class TranslateUIBingLCD implements Runnable, ITranslateUI, CommandListener {
+public class TranslateUIBingLCD implements Runnable, ITranslateUI, CommandListener, ItemCommandListener {
 
 	private Display display;
 	private Form form;
 	
 	private Command translateCmd = new Command("Translate", Command.OK, 1);
+	private Command setLangInCmd = new Command("Change in", Command.OK, 1);
+	private Command setLangOutCmd = new Command("Change out", Command.OK, 1);
+	private Command listChangeCmd = new Command("Change", Command.OK, 1);
 
 	private TranslateBingThread translateThread = new TranslateBingThread(this);
 
 	private boolean exiting;
 	private TextField textIn;
 	private TextField textOut;
+	private List listLangIn;
+	private List listLangOut;
+	private StringItem setLangInBtn;
+	private StringItem setLangOutBtn;
 	
 	public TranslateUIBingLCD() {
 		new Thread(this, "Main LCDUI Thread").start();
 	}
 
 	public void run() {
+		listLangIn = new List("Input language", List.EXCLUSIVE);
+		listLangOut = new List("Output language", List.EXCLUSIVE);
+		for(int i = 0; i < langs.length; i++) {
+			listLangIn.append(langs[i], null);
+			listLangOut.append(langs[i], null);
+		}
+		listLangIn.addCommand(listChangeCmd);
+		listLangOut.addCommand(listChangeCmd);
+		listLangIn.setSelectedIndex(0, true);
+		listLangOut.setSelectedIndex(4, true);
+		listLangIn.setCommandListener(this);
+		listLangOut.setCommandListener(this);
 		display = Display.getDisplay(TranslateBingMIDlet.midlet);
 		translateThread.start();
 		form = new Form("Bing Translate");
 		form.addCommand(translateCmd);
+		form.setCommandListener(this);
 		form.append(textIn = new TextField("", "", 512, TextField.ANY));
 		form.append(textOut = new TextField("", "", 512, TextField.ANY | TextField.UNEDITABLE));
-		// TODO
+		form.append(setLangInBtn = new StringItem("", "", StringItem.BUTTON));
+		setLangInBtn.setText("Input language: " + langs[listLangIn.getSelectedIndex()]);
+		setLangInBtn.setDefaultCommand(setLangInCmd);
+		setLangInBtn.setItemCommandListener(this);
+		form.append(setLangOutBtn = new StringItem("", "", StringItem.BUTTON));
+		setLangOutBtn.setText("Output language: " + langs[listLangOut.getSelectedIndex()]);
+		setLangOutBtn.setDefaultCommand(setLangOutCmd);
+		setLangOutBtn.setItemCommandListener(this);
+		display.setCurrent(form);
 	}
 
 	public String getText() {
@@ -48,13 +80,11 @@ public class TranslateUIBingLCD implements Runnable, ITranslateUI, CommandListen
 	}
 
 	public String getFromLang() {
-		// TODO
-		return "ru";
+		return langsAlias[listLangIn.getSelectedIndex()];
 	}
 
 	public String getToLang() {
-		// TODO
-		return "en";
+		return langsAlias[listLangOut.getSelectedIndex()];
 	}
 
 	public void sync() {
@@ -78,6 +108,20 @@ public class TranslateUIBingLCD implements Runnable, ITranslateUI, CommandListen
 
 	public void commandAction(Command c, Displayable d) {
 		if(c == translateCmd) translateThread.now();
+		if(c == listChangeCmd) {
+			setLangInBtn.setText("Input language: " + langs[listLangIn.getSelectedIndex()]);
+			setLangOutBtn.setText("Output language: " + langs[listLangOut.getSelectedIndex()]);
+			display.setCurrent(form);
+		}
+	}
+
+	public void commandAction(Command c, Item item) {
+		if(c == setLangInCmd) {
+			display.setCurrent(listLangIn);
+		} else if(c == setLangOutCmd) {
+			display.setCurrent(listLangOut);
+		}
+		
 	}
 
 }
