c81ead5dc9061b8a2113bc74de5821ba32d4d9c5
Shinovon <arman200711@gmail.com>
2021-10-03 16:23:00 +0500

    Update TranslateBingThread.java

--- src/cc/nnproject/translate/bing/TranslateBingThread.java
+++ src/cc/nnproject/translate/bing/TranslateBingThread.java
@@ -31,7 +31,10 @@ public class TranslateBingThread extends AbstractTranslateThread {
 		String req = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId=037C394ED1EA70440C3B5E07FA0A6A837DCE47A9&from=" + from + "&to=" + to + "&text=" + Util.encodeURL(s);
 	    try {
 			String r = Util.get(req);
-			if(r.charAt(1) == '"' && r.charAt(r.length() - 1) == '"') r = r.substring(2, r.length() - 1);
+			if(r.charAt(1) == '"' && r.charAt(r.length() - 1) == '"')
+				r = r.substring(2, r.length() - 1);
+			else if(r.charAt(0) == '"' && r.charAt(r.length() - 1) == '"')
+				r = r.substring(1, r.length() - 1);
 			r = StringUtils.replace(r, "\\u2029", "\n");
 			r = StringUtils.replace(r, "\\n", "\n");
 			r = StringUtils.cut(r, "\\r");
